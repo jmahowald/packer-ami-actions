b4901110d716fc53adf216e2577cc41a1b09bfb5
Josh Mahowald <joshua.mahowald@gmail.com>
2019-10-14 22:34:06 -0500

    use dockerize for all templating

--- image-cleanup/entrypoint.sh
+++ image-cleanup/entrypoint.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 POLICY_FILE=/tmp/ami-cleanup.yml
 export PATH=/home/custodian/.local/bin:$PATH
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
-j2 ami-cleanup.tpl.yml > $POLICY_FILE
+dockerize --template ami-cleanup.tpl.yml:$POLICY_FILE
+
+echo "Running policy:"
+cat $POLICY_FILE
 
 if [ "$INPUT_DRY_RUN" == "true" ]; then
     custodian run --dryrun -s out $POLICY_FILE
