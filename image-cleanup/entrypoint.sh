5c23c549970314aedb3acc20893a53cf2cebb396
Josh Mahowald <joshua.mahowald@gmail.com>
2019-10-15 18:56:44 -0500

    fix up where to find template file

--- image-cleanup/entrypoint.sh
+++ image-cleanup/entrypoint.sh
@@ -3,7 +3,7 @@
 POLICY_FILE=/tmp/ami-cleanup.yml
 export PATH=/home/custodian/.local/bin:$PATH
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
-dockerize --template ami-cleanup.tpl.yml:$POLICY_FILE
+dockerize --template /ami-cleanup.tpl.yml:$POLICY_FILE
 
 echo "Running policy:"
 cat $POLICY_FILE
