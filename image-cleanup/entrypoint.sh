bd9305f4d454391b071b91e5305f9c897cd966c1
Josh Mahowald <joshua.mahowald@gmail.com>
2019-10-14 20:20:56 -0500

    make the file writeable

--- image-cleanup/entrypoint.sh
+++ image-cleanup/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
+POLICY_FILE=/tmp/ami-cleanup.yml
 export PATH=/home/custodian/.local/bin:$PATH
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
-j2 ami-cleanup.tpl.yml > ami-cleanup.yml
+j2 ami-cleanup.tpl.yml > $POLICY_FILE
 
 if [ "$INPUT_DRY_RUN" == "true" ]; then
-    custodian run --dryrun -s out ami-cleanup.yml
+    custodian run --dryrun -s out $POLICY_FILE
 else
-    custodian run -s out ami-cleanup.yml
+    custodian run -s out $POLICY_FILE
 fi
 
