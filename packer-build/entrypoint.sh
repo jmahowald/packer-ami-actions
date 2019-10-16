474d2dc41e8351f760445af4fc57dd5ccc67dc7f
Josh Mahowald <joshua.mahowald@gmail.com>
2019-10-15 21:28:36 -0500

    seed in some more information

--- packer-build/entrypoint.sh
+++ packer-build/entrypoint.sh
@@ -1,20 +1,32 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 env
 pushd $INPUT_WORKING_DIR
 
 
-
+ADDITIONAL_INFO_FILE=${INPUT_ADDITIONAL_INFO_FILE:-/build-info.tpl.json}
 TEMPLATE_FILE=${INPUT_TEMPLATE_FILE:-$INPUT_TEMPLATE.json}
-dockerize -template /git-info.tpl.json:/git-info.json
+dockerize -template $ADDITIONAL_INFO_FILE:/build-info.json
+cat /build-info.json
 
-tagsmerge.sh $TEMPLATE_FILE /git-info.json > template.json
+# Yeah it's chintzy.  
+# But we need to a json merge of two sections.  I honestly
+# can't explain why this merges in both the builders and post-processors
+# section, but it seems to work so ¯\_(ツ)_/¯ 
+
+
+jq --argfile f1 /build-info.json \
+   --argfile f2 $TEMPLATE_FILE \
+   -n '$f1 * ($f1."builders"[]? * $f2."builders"[]? | { "builders": [.] } )' \
+   > packer-build.json
 
 echo "using merged file:"
-cat template.json
+cat packer-build.json
 
 if [ "$INPUT_VALIDATE_ONLY" == "true" ]; then
-    packer validate template.json
+    packer validate packer-build.json
 else
-    packer build template.json
+    packer build packer-build.json
+    echo "manifest:"
+    cat $TEMPLATE_FILE-manifest.json
 fi
