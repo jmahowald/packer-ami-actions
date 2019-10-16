72ae9390b321616047d6d305cfa5e2dca9aaa430
Josh Mahowald <joshua.mahowald@gmail.com>
2019-10-16 01:44:43 -0500

    dummy newline forcing?

--- packer-build/entrypoint.sh
+++ packer-build/entrypoint.sh
@@ -30,26 +30,13 @@ else
     echo "manifest:"
     cat $INPUT_TEMPLATE-manifest.json
     # Extract the ami from the manifest.  manifest looks like
-    # {
-    #   "builds": [
-    #     {
-    #       "name": "amazon-ebs",
-    #       "builder_type": "amazon-ebs",
-    #       "build_time": 1571194480,
-    #       "files": null,
-    #       "artifact_id": "us-east-1:ami-0f4b5f0233ea8ed95",
-    #       "packer_run_uuid": "63ed2471-48f4-0e06-38c1-c5efdabfc55c",
-    #       "custom_data": null
-    #     }
-    #   ],
-    #   "last_run_uuid": "63ed2471-48f4-0e06-38c1-c5efdabfc55c"
-    # }
 
     image_id=$(cat $INPUT_TEMPLATE-manifest.json \
      | jq -r '.builds[0].artifact_id' \
      | cut -d ":" -f 2)
     
+    
+    echo ""
     echo ::set-output name=image_id::$image_id
 
 fi
-
